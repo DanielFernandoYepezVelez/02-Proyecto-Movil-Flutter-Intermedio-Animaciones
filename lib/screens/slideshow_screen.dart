f4bb19a00d37ee1f262336696e89f058ea36e09e
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-10-29 10:54:51 -0500

    Iniciando El Diseño Del PageView

--- lib/screens/slideshow_screen.dart
+++ lib/screens/slideshow_screen.dart
@@ -8,7 +8,84 @@ class SlideShowScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svgs/slide-1.svg'),
+        child: Column(
+          children: [
+            Expanded(
+              child: _Slides(),
+            ),
+            _Dots(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  const _Slides({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide(svg: 'assets/svgs/slide-1.svg'),
+          _Slide(svg: 'assets/svgs/slide-2.svg'),
+          _Slide(svg: 'assets/svgs/slide-3.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  const _Slide({Key? key, required this.svg}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(30),
+      child: SvgPicture.asset(this.svg),
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  const _Dots({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(),
+          _Dot(),
+          _Dot(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12,
+      height: 12,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        shape: BoxShape.circle,
       ),
     );
   }
