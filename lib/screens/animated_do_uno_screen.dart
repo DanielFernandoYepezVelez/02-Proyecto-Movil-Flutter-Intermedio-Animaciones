42a3d88e6f0e3aa209b125c1edbc223c5382262a
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-11-12 16:20:40 -0500

    Animando La Primera Pantalla Con La Libreria De Animate Do

--- lib/screens/animated_do_uno_screen.dart
+++ lib/screens/animated_do_uno_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AnimatedDoUnoScreen extends StatelessWidget {
@@ -8,15 +10,28 @@ class AnimatedDoUnoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Animate Do'),
+        title: FadeIn(
+          delay: Duration(milliseconds: 500),
+          child: Text('Animate Do'),
+        ),
         actions: [
           IconButton(
             icon: FaIcon(FontAwesomeIcons.twitter),
             onPressed: () {},
           ),
-          IconButton(
-            icon: Icon(Icons.navigate_next),
-            onPressed: () {},
+          SlideInLeft(
+            from: 100,
+            child: IconButton(
+              icon: Icon(Icons.navigate_next),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (BuildContext context) => AnimatedDoUnoScreen(),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -24,22 +39,36 @@ class AnimatedDoUnoScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.new_releases, color: Colors.blue, size: 40),
-            Text(
-              'Titulo',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),
+            ElasticIn(
+              delay: Duration(milliseconds: 1100),
+              child: Icon(Icons.new_releases, color: Colors.blue, size: 40),
             ),
-            Text(
-              'Soy Un Texto Pequeño',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+            FadeInDown(
+              delay: Duration(milliseconds: 200),
+              child: Text(
+                'Titulo',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),
+              ),
             ),
-            Container(width: 220, height: 2, color: Colors.blue),
+            FadeInDown(
+              delay: Duration(milliseconds: 800),
+              child: Text(
+                'Soy Un Texto Pequeño',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+            ),
+            FadeInLeft(
+              delay: Duration(milliseconds: 1100),
+              child: Container(width: 220, height: 2, color: Colors.blue),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: FaIcon(FontAwesomeIcons.play),
-        onPressed: () {},
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          child: FaIcon(FontAwesomeIcons.play),
+          onPressed: () {},
+        ),
       ),
     );
   }
